dbacd6f96f3ca9f64a9162350e352a379a5f1672
abduakhads <160651255+abduakhads@users.noreply.github.com>
2026-03-07 08:01:12 +0500

    add logging for missing profile photos and personal chats in group message handler

--- bot/handlers/messages.py
+++ bot/handlers/messages.py
@@ -5,6 +5,7 @@ from bot import keyboards as kb
 from bot.handlers import common
 from bot.nsfw_worker import nsfw_queue
 from bot.database import Group
+from bot.settings import DEVELOPER_ID
 
 router = Router()
 
@@ -40,11 +41,31 @@ async def group_message_handler(message: types.Message, bot: Bot):
     user = await bot.get_chat(message.from_user.id)
     user_photo = user.photo
     if not user_photo:
-        print("No profile photo found.") #TODO: Log this event
+        # print("No profile photo found.") #TODO: Log this event
+
+        stripped_chat_id = str(message.chat.id).removeprefix("-100")
+        chat_link = f"https://t.me/{message.chat.username if message.chat.username else 'c/' + stripped_chat_id}"
+        message_link = chat_link + f"/{message.message_id}"
+
+        await bot.send_message(
+            chat_id=DEVELOPER_ID,
+            text=f"{message_link}\n\nNo profile photo found for user {user.full_name} in {message.chat.full_name} (id: {user.id}). Skipping NSFW check."
+        )
+
         return
 
     if not (user.personal_chat or user.bio):
-        print("No personal chat found.") #TODO: Log this event
+        # print("No personal chat found.") #TODO: Log this event
+
+        stripped_chat_id = str(message.chat.id).removeprefix("-100")
+        chat_link = f"https://t.me/{message.chat.username if message.chat.username else 'c/' + stripped_chat_id}"
+        message_link = chat_link + f"/{message.message_id}"
+
+        await bot.send_message(
+            chat_id=DEVELOPER_ID,
+            text=f"{message_link}\n\No personal chat found {user.full_name} in {message.chat.full_name} (id: {user.id}). Skipping NSFW check."
+        )
+
         return
 
     channel = await bot.get_chat(user.personal_chat.id)
